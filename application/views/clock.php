bd53d9a59832f4de38424a9de017e736972665f7
harishbhatt1099 <harish.bhatt1099@gmail.com>
2014-06-16 16:30:09 +0530

    Revert "git clone https://github.com/harishbhatt1099/report"
    This reverts commit e22e6a96496c15e3e2bcd8328fa4facf9f3504f6.

--- application/views/clock.php
+++
@@ -1,3 +0,0 @@
-<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/time_picker/src/jquery.ptTimeSelect.css" />
-<script type="text/javascript" src="<?php echo base_url();?>assets/time_picker/src/jquery.ptTimeSelect.js"></script>
-
